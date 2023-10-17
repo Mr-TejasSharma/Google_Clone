576775e3ef3da3875fd47eed743d2ff3c2ffa538
Mr-TejasSharma <tejas.sharma111002@gmail.com>
2023-10-17 12:48:49 +0530

    initial commit

---
+++ lib/responsive/mobile_screen_layout_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Mobiles extends StatelessWidget {
+  const Mobiles({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
